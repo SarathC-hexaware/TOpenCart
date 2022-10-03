7a16a18f9c9c05f02f4be4b239db248145ecc247
SarathCh-hexaware <sarathc2@hexaware.com>
2022-10-03 11:58:15 +0530

    tensai opencart

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tensaiopencart.eastus.cloudapp.azure.com/admin/');
+define('HTTP_CATALOG', 'http://tensaiopencart.eastus.cloudapp.azure.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://tensaiopencart.eastus.cloudapp.azure.com/admin/');
+define('HTTPS_CATALOG', 'https://tensaiopencart.eastus.cloudapp.azure.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_STORAGE', '/var/www/opencart/storage/');
+define('DIR_CATALOG', '/var/www/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'P&_2XFzKe/f_L$G#0jJ7');
+define('DB_DATABASE', 'opencart_3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'tsioc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tensaiopencart.eastus.cloudapp.azure.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://tensaiopencart.eastus.cloudapp.azure.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/catalog/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_STORAGE', '/var/www/opencart/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'P&_2XFzKe/f_L$G#0jJ7');
+define('DB_DATABASE', 'opencart_3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'tsioc_');
